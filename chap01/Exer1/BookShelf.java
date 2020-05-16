d58fc7d0e4fd9cbf1b012f6acd5173b107746432
INABA Akira <inaba.akira@gmail.com>
2020-05-16 13:27:26 +0900

    [Chap01] 練習問題 問題 1-1 終了

---
+++ chap01/Exer1/BookShelf.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class BookShelf implements Aggregate {
+    private ArrayList<Book> books;
+    private int last = 0;
+    public BookShelf() {
+        this.books = new ArrayList<Book>();
+    }
+    public Book getBookAt(int index) {
+        return books.get(index);
+    }
+    public void appendBook(Book book) {
+        this.books.add(book);
+    }
+    public int getLength() {
+        return books.size();
+    }
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
